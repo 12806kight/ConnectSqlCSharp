611ee57e98fdf68f00fd91657c8b506b338696a8
Brewer <12806kight@gmail.com>
2024-01-20 16:01:37 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect2Mysql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = "localhost";
+            string database = "test";
+            string username = "root";
+            string password = "password";
+            string constring = "HOST=" + host + ";" + "DATABASE=" + database + ";" +
+                "UID=" + username + ";" + "PASSWORD=" + password + ";" + "Port=" + 3306 + ";";
+            MySqlConnection conn = new MySqlConnection(constring);
+            conn.Open();
+            string query = "select * from person";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["FirstName"]);
+            }
+        }
+    }
+}
